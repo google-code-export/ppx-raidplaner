485325774cb5aedb307b963fb5031850f8ff9f32
arne.claus@googlemail.com <arne.claus@googlemail.com@05db9da3-c08c-0c20-3c61-cb62523a2622>
2012-09-24 19:24:51 +0000

    TAG: 0.9.5c release

---
+++ tags/0.9.5c/lib/private/message_query_newraid.php
@@ -0,0 +1,45 @@
+<?php
+
+function msgQueryNewRaidData( $Request )
+{
+	if ( ValidRaidlead() )
+    {
+    	$Connector = Connector::GetInstance();
+    	
+    	// Settings
+    	
+    	$NewRaidSettings = $Connector->prepare("Select Name, IntValue FROM `".RP_TABLE_PREFIX."Setting`");
+    	
+    	if ( !$NewRaidSettings->execute() )
+        {
+        	postErrorMessage( $NewRaidSettings );
+        }
+        else
+        {
+        	echo "<settings>";
+	        	
+        	$OfInterest = array( "RaidSize", "RaidStartHour", "RaidStartMinute", "RaidEndHour", "RaidEndMinute" );
+        	while ( $Data = $NewRaidSettings->fetch( PDO::FETCH_ASSOC ) )
+	        {
+	        	if ( in_array($Data["Name"], $OfInterest) )
+	        	{
+	        		echo "<".$Data["Name"].">".$Data["IntValue"]."</".$Data["Name"].">";
+	        	}
+	        }
+	        
+	        echo "</settings>";
+        }
+        
+        $NewRaidSettings->closeCursor();
+        
+        // Locations
+        
+        msgQueryLocations($Request);
+    }
+    else
+    {
+        echo "<error>".L("AccessDenied")."</error>";
+    }
+}
+   
+?>
